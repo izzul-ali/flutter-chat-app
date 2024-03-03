e062274333d6b10cd334ae51f0c441ef9b754f2c
izzul-ali <izzuldev@gmail.com>
2024-03-03 17:31:08 +0700

    chore: update router

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/core/configs/firebase_options.dart';
 import 'package:flutter_chat_app/core/constant/theme.dart';
-import 'package:flutter_chat_app/core/route/routing.dart';
+import 'package:flutter_chat_app/core/route/route.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
@@ -16,18 +16,20 @@ Future<void> main() async {
   );
 }
 
-class ChatApp extends StatelessWidget {
+class ChatApp extends ConsumerWidget {
   const ChatApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(goRouterProvider);
+
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: 'Chat App',
         theme: themeData,
-        routerConfig: routes,
+        routerConfig: router,
       ),
     );
   }
